5e2cd41890ec5fb950ea5dacb55b824f12090b51
j0verton <overtonje@gmail.com>
2020-08-27 12:23:26 -0500

    started sort challenge

--- src/scripts/catalog.js
+++ src/scripts/catalog.js
@@ -1,5 +1,7 @@
 export const catalog = (harvestArray) => {
     const foodTarget = document.querySelector(".container")
+    harvestArray.sort();
+    console.log("sorted?",harvestArray)
     for (const food of harvestArray) {
         foodTarget.innerHTML += `<section class="plant">${food.type}</section>`
 
